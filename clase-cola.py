cecd2d4d5ce99d52e6208fac53c75aeb1760ad1b
suarez <suarezjesica751@gmail.com>
2019-08-29 22:39:07 -0300

    29/08/2019

--- clase-cola.py
+++ clase-cola.py
@@ -9,23 +9,38 @@ class Cola:
         return self.items.pop()
     def tamano(self):
         return len(self.items)
-  
     def imprimir(self):
         print (self.items)
-    
     def vaciar(self):
         while not (self.estavacia()):
             self.avanzar()
-            
     def dar_vuelta(self):
         self.items == (self.items[::-1])
         
-        
-        
-        
-c=Cola()
-(c.agregar(6))
-(c.agregar(4))
-(c.dar_vuelta())
-(c.imprimir())
-(c.vaciar()):
+'''Implenetacion de cola'''
+#c=Cola()
+#(c.agregar(6))
+#(c.agregar(4))
+#(c.dar_vuelta())
+#(c.vaciar())
+#(c.imprimir())
+
+
+
+'''Recibe una cola mueve sus elementos a una cola nueva'''
+
+def mover(cola):
+    colaNueva=Cola()
+    while not cola.estavacia():
+        colaNueva.agregar(cola.avanzar())
+    return colaNueva
+
+cola=Cola()
+(cola.agregar(2))
+(cola.agregar(3))
+(cola.agregar(9))
+(cola.imprimir())
+#mover(cola).imprimir()
+print(cola.estavacia())
+
+
