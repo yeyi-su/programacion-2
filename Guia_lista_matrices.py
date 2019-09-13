274b7daac206c055bcdbd439caf5f64fd05cd6e5
suarez <suarezjesica751@gmail.com>
2019-09-13 16:01:18 -0300

    13/09/19

---
+++ Guia_lista_matrices.py
@@ -0,0 +1,29 @@
+#miLista = [1024, 3, True, 6.5] # crea una lista con elementos  
+#miLista.append(False) # Agrega el elmento False por la derecha
+#print(miLista) #Imprimie la lista
+#miLista.insert(2,4.5) # Agrega un nuevo elemento a la lista en la posicion 2
+#print(miLista) # Imprime la lista
+#print(miLista.pop()) # Elimina el ultimo elemento  de la lista
+#print(miLista) # Imprime la lista
+#print(miLista.pop(1)) # Elimina el elemento en la posicion 1
+#print(miLista) # Se imprime la lista
+#miLista.pop(2) # Elimina y devuelve el elemento en la posicion 2
+#print(miLista) # Se imprime la lista
+#miLista.sort() # Se ordenan los elementos del mas chico al mas grande
+#print(miLista) # Se imprime la lista
+#miLista.reverse() # Devuelve la lista al reves
+#print(miLista) # Se imprime la lista
+#print(miLista.count(6.5)) # Devuelve la cantidad del mismo elementos que tiene 
+#print(miLista.index(4.5)) # Devuelve el indice en que se encuentra el elemento
+#miLista.remove(6.5) # Elimina el elemento de la lista
+#print(miLista) # Imprime la lista
+#del miLista[0] # Elimina el elmento del indice 0
+#print(miLista) #imprime la lista
+
+a=[3,3,3,3,3,3,3,3,3]
+for in in range (10):
+    print(a[i])
+
+# a) NO se puede ejecutar
+# b) El problema esta en la linea del for
+# c) 
